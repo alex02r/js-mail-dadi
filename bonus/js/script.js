ac6a9fd7d2fca9b73a3031915b21047b6624b8e2
alex02r <alex.giochi02@gmail.com>
2023-11-23 17:45:12 +0100

    [UPGRADE]Add check on email

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,22 +1,27 @@
 //SCRIPT FOR MAIL
-let emails= ["example@gmail.com","example@libero.it","example@icloud.com","example@hotmail.com"];
+let emails= ["example@gmail.com", "example@libero.it", "example@icloud.com", "example@hotmail.com"];
 
-let email = document.getElementById('email').value;
+let content_email = document.getElementById("content-email");
 let accedi = document.getElementById('accedi');
 
 let flag = false;
 
-accedi.addEventListener('click', function( ){
-    for (let i = 0; i < emails.length; i++) {
+accedi.addEventListener('click', function(){
     
+    let email = document.getElementById('email').value;
+    for (let i = 0; i < emails.length; i++) {
         if(email == emails[i]){
-            console.log('La tua email è presente');
             flag = true;
         }
     }
     
-    if(!flag){
+    if(flag == false){
+        document.getElementById("email").value = "";
         console.log('L\'email che hai inserito non è presente');
+        console.log(email);
+    }else{
+        console.log('La tua email è presente');
+        content_email.classList.add('d-none');
     }
 })
 
