fc1fc2d0484677c39c7388e73d829841fb98f0e0
alex02r <alex.giochi02@gmail.com>
2023-11-23 14:52:22 +0100

    [UPGRADE]Aggointo il for con controlli sull email

--- mail/js/script.js
+++ mail/js/script.js
@@ -2,3 +2,21 @@ let emails= ["example@gmail.com","example@libero.it","example@icloud.com","examp
 
 let email = prompt('Inserisci l\'email');
 
+//inizialissiamo una variabile che andiamo a controllare dopo il ciclo for 
+let flag = false;
+for (let i = 0; i < emails.length; i++) {
+    //inizializziamo il for in modo da leggere ogni elemento dell'array
+    //controllliamo se "i" elemento contiente l'email inserita dall'utente
+    if(email == emails[i]){
+        //siamo qui perchè l'email è presente nell'array
+        console.log('La tua email è presente');
+        flag = true;
+    }
+}
+
+//controlliamo se il ciclo for non ha trovato nessun email
+if(!flag){
+    //se flag è false, significa che il ciclo for non ha trovato nessun email presente nell'array
+    //quindi facciamo visualizzare il messaggio
+    console.log('L\'email che hai inserito non è presente');
+}
