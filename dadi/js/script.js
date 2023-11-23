c62117d178836c8e1b75f97d284b4c35b9f819eb
alex02r <alex.giochi02@gmail.com>
2023-11-23 16:20:43 +0100

    [UPGRADE]Chiedere all'utente quanti round giocare

---
+++ dadi/js/script.js
@@ -0,0 +1,7 @@
+//variabilie per il punteggio
+let score = 0;
+
+//chiediamo all'utente quanti round vuole giocare
+let rounds = parseInt(prompt('Inserisci quanti round vuoi giocare'));
+
+//ciclo for per far giocare i round scelti in precedenza
