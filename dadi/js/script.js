922cf1286cd1a813b92b6e00d8c951883be37fa3
alex02r <alex.giochi02@gmail.com>
2023-11-23 16:26:55 +0100

    [UPGRADE]Create le variabili per generare i numeri

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -6,5 +6,10 @@ let rounds = parseInt(prompt('Inserisci quanti round vuoi giocare'));
 
 //ciclo for per far giocare i round scelti in precedenza
 for(i=0; i <= rounds; i++){
+    //numero da 1 a 6 del pc
+    let number_pc = Math.floor(Math.random()* (6 - 1) + 1);
+    
+    //numero da 1 a 6 dell'utente
+    let number_user = Math.floor(Math.random()* (6 - 1) + 1);
     
 }
