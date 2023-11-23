b2ce46f54dd72375dc13aefbead42056d19598ec
alex02r <alex.giochi02@gmail.com>
2023-11-23 18:04:29 +0100

    [UPGRADE]Add function for game

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -2,12 +2,14 @@
 let emails= ["example@gmail.com", "example@libero.it", "example@icloud.com", "example@hotmail.com"];
 
 let content_email = document.getElementById("content-email");
+let dadi = document.getElementById("col-8");
+
 let accedi = document.getElementById('accedi');
 
 let flag = false;
 
 accedi.addEventListener('click', function(){
-    
+
     let email = document.getElementById('email').value;
     for (let i = 0; i < emails.length; i++) {
         if(email == emails[i]){
@@ -22,6 +24,30 @@ accedi.addEventListener('click', function(){
     }else{
         console.log('La tua email è presente');
         content_email.classList.add('d-none');
+        dadi.classList.remove('d-none');
+        dadi.classList.add('d-flex');
     }
-})
+});
 
+
+let play = document.getElementById("play");
+
+play.addEventListener('click', function(){
+
+    let score = 0;
+    let rounds = document.getElementById("rounds").value;
+    
+    for(i=1; i <= rounds; i++){
+        let number_pc = Math.floor(Math.random()* (6 - 1) + 1);
+        let number_user = Math.floor(Math.random()* (6 - 1) + 1);
+
+        if(number_user > number_pc){
+            score += 1;
+            //messaggio nell html che abbiamo bitno il round n "i"
+    
+        }else{
+            //messaggio che abbiamo perso il round n "i"
+        }
+    }
+   //messaggio punteggio totale
+});
