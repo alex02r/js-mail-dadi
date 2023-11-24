685fb7820f1bd9991453fc501634d7035197927a
alex02r <alex.giochi02@gmail.com>
2023-11-24 10:16:51 +0100

    [FIX]Rimozione del ciclo for

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,27 +1,18 @@
 //variabilie per il punteggio
 let score = 0;
 
-//chiediamo all'utente quanti round vuole giocare
-let rounds = parseInt(prompt('Inserisci quanti round vuoi giocare'));
-
-//ciclo for per far giocare i round scelti in precedenza
-for(i=1; i <= rounds; i++){
-    //numero da 1 a 6 del pc
-    let number_pc = Math.floor(Math.random()* (6 - 1) + 1);
+//numero da 1 a 6 del pc
+let number_pc = Math.floor(Math.random()* (6 - 1) + 1);
     
-    //numero da 1 a 6 dell'utente
-    let number_user = Math.floor(Math.random()* (6 - 1) + 1);
+//numero da 1 a 6 dell'utente
+let number_user = Math.floor(Math.random()* (6 - 1) + 1);
 
-    //controlliamo chi ha vinto
-    if(number_user > number_pc){
-        //siamo qui perchè il numero dell'utente è maggiore del numero del computer
-        //quindi ha vinto l'utente e incrementiamo il punteggio
-        score += 1;
-        console.log('WIN!!!! round n',i);
+//controlliamo chi ha vinto
+if(number_user > number_pc){
+    //siamo qui perchè il numero dell'utente è maggiore del numero del computer
+    console.log('WIN!!!!');
 
-    }else{
-        //siamo qui perchè il numero del computer era maggiore
-        console.log('Lose! round n', i);
-    }
+}else{
+    //siamo qui perchè il numero del computer era maggiore
+    console.log('Lose!');
 }
-console.log('Il tuo punteggio totale è ', score,'su ', rounds);
